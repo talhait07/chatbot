47c1153d77a6284c9ccf50eb81382fad3c7f2f66
monsur.it07 <monsur.it07@gmail.com>
2020-04-04 20:25:14 +0600

    initial commit

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,6 @@
+class RoomsController < ApplicationController
+  def index
+  end
+
+
+end
---
+++ config/webpack/environment.js
@@ -0,0 +1,13 @@
+const { environment } = require('@rails/webpacker')
+
+const webpack = require('webpack')
+// Add an additional plugin of your choosing : ProvidePlugin
+environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
+        $: 'jquery',
+        JQuery: 'jquery',
+        jquery: 'jquery',
+        'window.Tether': "tether",
+        Popper: ['popper.js', 'default'], // for Bootstrap 4
+    })
+)
+module.exports = environment
