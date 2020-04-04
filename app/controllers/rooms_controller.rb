383d0986162efbdd1436b2e6abd2a0243f5c916a
monsur.it07 <monsur.it07@gmail.com>
2020-04-04 20:34:45 +0600

    create new room action and views added

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,9 +1,33 @@
 class RoomsController < ApplicationController
+
+  before_action :load_rooms
+
   def index
-    @rooms = Room.all
   end
 
   def new
     @room = Room.new
+  end
+
+  def create
+    @room = Room.new permitted_parameters
+
+    if @room.save
+      flash[:success] = "Room #{@room.name} was created successfully"
+      redirect_to rooms_path
+    else
+      render :new
+    end
+  end
+
+  protected
+
+  def load_rooms
+    @rooms = Room.all
+    @room = Room.find(params[:id]) if params[:id]
+  end
+
+  def permitted_parameters
+    params.require(:room).permit(:name)
   end
 end
